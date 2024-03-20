408546b8e4b3da428b98d8e97edbb0d1986ba635
이재영 <101492831+Finder16@users.noreply.github.com>
2024-03-20 14:05:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.secret_key = b'secret'
 
 users = {
-    "admin": "i don't make a mistake",
+    "admin": "9b71d224bd62f3785d96d46ad3ea3d73319bfbc2890caadae2dff72519673ca72323c3d99ba5c11d7c7acc6e14b8c5da0c4663475c2e5c3adef46f73bcdec04d",
     "guest": "guest"
 }
 
